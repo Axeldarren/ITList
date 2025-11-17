a074faf392eba52c0ebe94f2e773b9e1a37438bf
Axel <axeldarren.suryanto@gmail.com>
2025-11-17 08:39:53 +0700

    update cors setting

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,7 +36,7 @@ const app = express();
 app.set('trust proxy', 1);
 
 const corsOptions = {
-  origin: "*",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Encrypted'],
